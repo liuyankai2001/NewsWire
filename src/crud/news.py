ecf7f5f64591d6662611fde01af0c2d4a7e78f52
liuyankai <liuyankai23@mails.ucas.ac.cn>
2026-06-18 16:33:21 +0800

    获取新闻列表

--- src/crud/news.py
+++ src/crud/news.py
@@ -1,9 +1,19 @@
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.models.news import CateGory
+from src.models.news import CateGory,News
 
 async def get_categories(db:AsyncSession,skip:int=0,limit:int=100):
     stmt = select(CateGory).offset(skip).limit(limit)
     results = await db.execute(stmt)
     categories = results.scalars().all()
     return categories
+
+async def get_news_list(db:AsyncSession,category_id:int,skip:int=0,limit=10):
+    stmt = select(News).where(News.category_id==category_id).offset(skip).limit(limit)
+    result = await db.execute(stmt)
+    return result.scalars().all()
+
+async def get_news_count(db:AsyncSession,category_id:int):
+    stmt = select(func.count(News.id)).where(News.category_id==category_id)
+    result = await db.execute(stmt)
+    return result.scalar_one()
--- src/routers/news.py
+++ src/routers/news.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi.params import Depends
+from fastapi.params import Depends,Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.crud import news
 from src.config.db_conf import get_database
@@ -13,5 +13,26 @@ async def get_categories(skip:int=0,limit:int=100,db:AsyncSession=Depends(get_da
         "msg": "获取分类成功",
         "code":200,
         "data":categories
+    }
+
+@router.get("/list")
+async def get_news(
+        category_id:int = Query(...,alias="categoryId"),
+        page:int = 1,
+        page_size:int = Query(10,alias="pageSize",le=100),
+        db:AsyncSession = Depends(get_database)
+):
+    offset = (page-1)*page_size
+    news_list = await news.get_news_list(db,category_id,offset,page_size)
+    total = await news.get_news_count(db,category_id)
+    has_more = (offset+len(news_list)) < total
+    return {
+        "code":200,
+        "message":"获取新闻列表成功",
+        "data":{
+            "list":news_list,
+            "total":total,
+            "hasMore":has_more
+        }
     }
 
