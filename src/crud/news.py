305910c5a8560cb003448499a21348c293d28861
liuyankai <liuyankai23@mails.ucas.ac.cn>
2026-06-18 16:54:14 +0800

    获取新闻详情并更新浏览量

--- src/crud/news.py
+++ src/crud/news.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.news import CateGory,News
 
@@ -17,3 +17,13 @@ async def get_news_count(db:AsyncSession,category_id:int):
     stmt = select(func.count(News.id)).where(News.category_id==category_id)
     result = await db.execute(stmt)
     return result.scalar_one()
+
+async def get_news_detail(db:AsyncSession,news_id):
+    stmt = select(News).where(News.id==news_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+async def increase_news_views(db:AsyncSession,news_id):
+    stmt = update(News).where(News.id==news_id).values(views = News.views+1)
+    result = await db.execute(stmt)
+    return result.rowcount>0
--- src/routers/news.py
+++ src/routers/news.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends,Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.crud import news
@@ -36,3 +36,26 @@ async def get_news(
         }
     }
 
+@router.get("/detail")
+async def get_news_detail(news_id:int=Query(..., alias="id"),db:AsyncSession = Depends(get_database)):
+    news_detail = await news.get_news_detail(db,news_id)
+    if not news_detail:
+        raise HTTPException(status_code=404,detail="新闻不存在")
+    views_response = await news.increase_news_views(db,news_id)
+    if not views_response:
+        raise HTTPException(status_code=404, detail="更新浏览量失败")
+    return {
+        "message":"success",
+        "code":200,
+        "data":{
+            "id":news_detail.id,
+            "title":news_detail.title,
+            "content":news_detail.content,
+            "image":news_detail.image,
+            "author":news_detail.author,
+            "publishTime":news_detail.publish_time,
+            "categoryId":news_detail.category_id,
+            "views":news_detail.views,
+            "relationNews":[],
+        }
+    }
