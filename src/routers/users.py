1eec5b74b3733d96e7e3b275b3e3a8fb948fe992
liuyankai <liuyankai23@mails.ucas.ac.cn>
2026-06-22 14:02:07 +0800

    完成用户登录功能

--- src/routers/users.py
+++ src/routers/users.py
@@ -38,3 +38,13 @@ async def register(user_data:UserRequest,db:AsyncSession=Depends(get_database),)
     # }
     response_data = UserAuthResponse(token=token,userInfo=UserInfoResponse.model_validate(user))
     return success_response(messagse="注册成功",data=response_data)
+
+@router.post("/login")
+async def login(user_data:UserRequest,db:AsyncSession=Depends(get_database)):
+    # 登录逻辑：验证用户是否存在 -> 验证密码 -> 生成token -> 相应结果
+    user = await users.authenticate_user(db,user_data.username,user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="用户名或密码错误")
+    token = await  users.create_token(db,user.id)
+    response_data = UserAuthResponse(token=token, user_info=UserInfoResponse.model_validate(user))
+    return success_response("登录成功",data=response_data)
--- src/utils/security.py
+++ src/utils/security.py
@@ -6,3 +6,8 @@ pwd_context = CryptContext(schemes=['bcrypt'],deprecated="auto")
 # 密码加密
 def get_hash_password(password:str):
     return pwd_context.hash(password)
+
+
+# 验证密码
+def verify_password(plain_password,hashed_password):
+    return pwd_context.verify(plain_password,hashed_password)
